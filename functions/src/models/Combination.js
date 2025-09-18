371be99593278a0cc1e34d8ef3cdf0b33095a545
Carlos <casalgado86@gmail.com>
2025-09-18 09:27:19 -0500

    updating models

--- functions/src/models/Combination.js
+++ functions/src/models/Combination.js
@@ -2,7 +2,7 @@ const { generateId, capitalize } = require('../utils/helpers');
 
 class Combination {
   constructor(data = {}) {
-    this.id = data.id;
+    this.id = data.id || generateId();
     this.selection = (data.selection || []).map(item => capitalize(item));
     this.name = capitalize(data.name) || '';
     this.basePrice = data.basePrice || 0;
